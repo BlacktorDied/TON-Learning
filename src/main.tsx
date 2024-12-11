d1ef73ab49faee92f0a509896f28346a26a4a15b
BlacktorDied <alex.dozorin@serokell.io>
2024-12-11 20:01:57 +0200

    Workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // This manifest is used temporarily for development purposes
-const manifestUrl =
-  "https://raw.githubsercontent.com/ton-community/tutorial/main/03-client/test/public/tonconnect-maniefst.json";
+const manifestUrl = "https://blacktordied.github.io/TON-Learnin/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
