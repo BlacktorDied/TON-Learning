005a737ded9f313f1e5c988bf10034d36c0feb5e
BlacktorDied <alex.dozorin@serokell.io>
2024-12-12 09:59:33 +0200

    fix up 3

--- src/App.tsx
+++ src/App.tsx
@@ -5,53 +5,75 @@ import { useTonConnect } from "./hooks/useTonconnect";
 import { fromNano } from "ton-core";
 
 function App() {
-    const {
-        contract_address,
-        counter_value,
-        contract_balance,
-        sendIncrement,
-        sendDeposit,
-		sendWithdrawalRequest,
-    } = useMainContract();
+  const {
+    contract_address,
+    counter_value,
+    contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest,
+  } = useMainContract();
 
-    const { connected } = useTonConnect();
-    
-    return (
-        <div>
-            <div>
-                <TonConnectButton />
-            </div>
-            <div>
-                <div className="Card">
-                    <b>Our contract Address</b>
-                    <div className="Hint">
-                        {contract_address?.slice(0, 30) + "..."}
-                    </div>
-                    <b>Our contract Balance</b>
-                    {contract_balance && (
-                        <div className="Hint">{fromNano(contract_balance)}</div>
-                    )}
-                </div>
+  const { connected } = useTonConnect();
 
-                <div className="Card">
-                    <b>Counter Value</b>
-                    <div>{counter_value ?? "Loading..."}</div>
-                </div>
-            </div>
-
-            {connected && (
-                <a onClick={() => sendIncrement()}> Increment by 5</a>
-            )}
-
-            <br />
-
-            {connected && <a onClick={() => sendDeposit()}>Request deposit of 1 TON</a>}
-
-			<br />
-
-			{connected && <a onClick={() => sendWithdrawalRequest()}>Request 0.7 TON withdrawal</a>}
+  return (
+    <div>
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className='Card'>
+          <b>Our contract Address</b>
+          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+          <b>Our contract Balance</b>
+          <div className='Hint'>
+            {contract_balance && fromNano(contract_balance)}
+          </div>
         </div>
-    );
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{counter_value ?? "Loading..."}</div>
+        </div>
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendIncrement();
+            }}
+          >
+            Increment by 5
+          </a>
+        )}
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendDeposit();
+            }}
+          >
+            Request deposit of 1 TON
+          </a>
+        )}
+
+        <br />
+
+        {connected && (
+          <a
+            onClick={() => {
+              sendWithdrawalRequest();
+            }}
+          >
+            Request 0.7 TON withdrawal
+          </a>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App; 
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import App from "./App.tsx";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // This manifest is used temporarily for development purposes
-const manifestUrl = "https://blacktordied.github.io/TON-Learning/tonconnect-manifest.json";
+const manifestUrl =
+  "https://blacktordied.github.io/TON-Learning/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
