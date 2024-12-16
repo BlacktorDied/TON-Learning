c54501af588de62e19640ecb86ad0062265e8ce3
BlacktorDied <alex.dozorin@serokell.io>
2024-12-16 13:56:29 +0100

    Finished TMA project

--- TMA_exercise/src/App.tsx
+++ TMA_exercise/src/App.tsx
@@ -1,32 +1,112 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+import {
+    useTonAddress,
+    SendTransactionRequest,
+    useTonConnectUI,
+    useTonWallet,
+} from "@tonconnect/ui-react";
+import TonWeb from "tonweb";
+
 export default function App() {
+    const userFriendlyAddress = useTonAddress();
+    const rawAddress = useTonAddress(false);
+    const wallet = useTonWallet();
+
+    const tonweb = new TonWeb(
+        new TonWeb.HttpProvider("https://testnet.toncenter.com/api/v2/jsonRPC")
+    );
+    const [balance, setBalance] = useState<string>();
+
+    useEffect(() => {
+        if (!wallet) return;
+        const getBalance = async () =>
+            setBalance(await tonweb.getBalance( wallet.account.address));
+        getBalance();
+    }, [wallet]);
+
+    const [tonConnectUI] = useTonConnectUI();
+
+    const [address, setAddress] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const transaction: SendTransactionRequest = {
+        validUntil: Date.now() + 5 * 60 * 1000,
+        messages: [
+            {
+                address: address,
+                amount: amount,
+            },
+        ],
+    };
+
     return (
         <>
-            <h1>TON Mini App</h1>
+            <h1>Telegram Mini App</h1>
 
-            <div className="buttonWrapper">
-                <button id="connect-wallet">Connect Wallet</button>
-                <p id="wallet-address">Wallet: Not connected</p>
-                <button id="disconnect">Disconnect</button>
-            </div>
+            {userFriendlyAddress ? (
+                <button
+                    id="disconnect-wallet"
+                    onClick={() => tonConnectUI.disconnect()}
+                >
+                    Disconnect Wallet
+                </button>
+            ) : (
+                <button
+                    id="connect-wallet"
+                    onClick={() => tonConnectUI.openModal()}
+                >
+                    Connect Wallet
+                </button>
+            )}
 
-            <div className="data">
-                <div className="Card">
-                    <b>Wallet Address</b>
-                    <p id="address">Loading...</p>
+            {userFriendlyAddress && (
+                <div>
+                    <div className="address">
+                        <p>
+                            <b>User-friendly address:</b> {userFriendlyAddress}
+                        </p>
+                        <p>
+                            <b>Raw address:</b> {rawAddress}
+                        </p>
+                    </div>
+
+                    <div className="balance">
+                        <p>
+                            <b>Wallet Balance:</b> {balance ? TonWeb.utils.fromNano(balance) : "Loading..."} TON
+                        </p>
+                    </div>
+
+                    <div className="send-transaction">
+                        <label htmlFor="address">
+                            Address:
+                            <input
+                                type="text"
+                                id="address"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+                        </label>
+                        <label htmlFor="amount">
+                            Amount (in nano):
+                            <input
+                                type="text"
+                                id="amount"
+                                value={amount}
+                                onChange={(e) => setAmount(e.target.value)}
+                            />
+                        </label>
+                        <button
+                            onClick={() =>
+                                tonConnectUI.sendTransaction(transaction)
+                            }
+                        >
+                            Send transaction
+                        </button>
+                    </div>
                 </div>
-
-                <div className="Card">
-                    <b>Balance</b>
-                    <p id="balance">Loading...</p>
-                </div>
-
-                <div className="Card">
-                    <b>Counter Value</b>
-                    <p id="counterValue">Loading...</p>
-                </div>
-            </div>
+            )}
         </>
     );
 }
--- TMA_exercise/src/main.tsx
+++ TMA_exercise/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import WebApp from '@twa-dev/sdk'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
+
+WebApp.ready()
+
+const manifestUrl =
+  "https://blacktordied.github.io/TON-Learning/TMA_exercise/tonconnect-manifest.json";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </StrictMode>,
+  </TonConnectUIProvider>,
 )
